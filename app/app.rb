66b6eec645e143b78eb43e5ae19fa44556b582b0
Mutsa <mutsa.evelyn@hotmail.com>
2017-07-25 17:37:29 +0100

    Completed User Story : As a time-pressed user So that I can save a website I would like to add the site's address and title to my bookmark manager

--- app/app.rb
+++ app/app.rb
@@ -8,4 +8,13 @@ class BookmarkManager < Sinatra::Base
     @links = Link.all
     erb :'links/index'
   end
+
+  post '/links' do
+    Link.create(url: params[:url], title: params[:title])
+    redirect '/links'
+  end
+
+  get '/links/new' do
+    erb :'links/new'
+  end
 end
