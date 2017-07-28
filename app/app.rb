73c40a3325a58c5d8737a7780431c2181a22195d
Mutsa <mutsa.evelyn@hotmail.com>
2017-07-28 12:42:55 +0100

    Completed step 20: created functionality for handling the situation when the passwords do not match

--- app/app.rb
+++ app/app.rb
@@ -1,8 +1,10 @@
 ENV["RACK_ENV"] ||= "development"
 require 'sinatra/base'
+require 'sinatra/flash'
 require_relative 'data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
+  register Sinatra::Flash
   enable :sessions
   set :session_secret, 'super secret'
 
@@ -31,15 +33,21 @@ class BookmarkManager < Sinatra::Base
   end
 
   get '/users/new' do
+    @user = User.new
     erb :'users/new'
   end
 
   post '/users' do
-    user = User.create(email: params[:email],
-                       password: params[:password],
-                       password_confirmation: params[:password_confirmation])
-   session[:user_id] = user.id
-   redirect to('/links')
+    @user = User.new(email: params[:email],
+                    password: params[:password],
+                    password_confirmation: params[:password_confirmation])
+   if @user.save
+     session[:user_id] = @user.id
+     redirect to('/links')
+   else
+     flash.now[:notice] = "Password and confirmation password do not match"
+     erb :'users/new'
+   end
  end
 
  helpers do
